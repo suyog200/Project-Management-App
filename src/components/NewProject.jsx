b37f61f54dc5beb28c043ff06e15362017b88cfe
suyog200 <suygchari@gmail.com>
2024-02-28 15:47:49 +0530

    added functionality to save project data, and open form when +add project

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,20 +1,40 @@
+import { useRef } from "react"; 
 import Input from "./Input";
 
-export default function NewProject() {
+export default function NewProject({onAdd}) {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        });
+    };
+
     return (
         <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
             <li>
-            <button className="text-stone-800 hover:text-stone-950 border-2 rounded-md px-6 py-2 hover:border-stone-700">cancel</button>
+            <button className="text-stone-800 hover:text-stone-950 border-2 rounded-md px-6 py-2 hover:border-stone-900">cancel</button>
             </li>
             <li>
-            <button className="px-6 py-2 rounded-md bg-stone-800 hover:bg-stone-900 text-stone-50">save</button>
+            <button 
+            className="px-6 py-2 rounded-md bg-stone-800 hover:bg-stone-900 text-stone-50"
+            onClick={handleSave}
+            >save</button>
             </li>
         </menu>
             <div>
-                <Input label='title' />
-                <Input label='Description' textarea/>
-                <Input label='Due Date' />
+                <Input type='text' ref={title} label='title' />
+                <Input ref={description} label='Description' textarea/>
+                <Input type='date' ref={dueDate} label='Due Date' />
             </div>
         </div>
     );
---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,15 @@
+import NoProjectImage from '../assets/no-projects.png';
+import Button from './Button';
+
+export default function NoProjectSelected({onStartAddProject}) {
+    return (
+        <div className="mt-24 text-center w-2/3">
+            <img src={NoProjectImage} alt="" className='w-16 h-16 mx-auto object-contain'/>
+            <h2 className='text-xl font-bold text-stone-500 my-4'>No project is selected!</h2>
+            <p className='text-stone-400 mb-4'>Select a project or get started with new one</p>
+            <p className='mt-8'>
+                <Button onClick={onStartAddProject}>Create a new project</Button>
+            </p>
+        </div>
+    );
+}
